5f429c82ee020fd1bbd9c4f384f42f3a558148a6
taka <taka11japan@gmail.com>
2025-11-08 17:50:52 +0900

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
+  // ここで Next.js が使用するワークスペースルートを明示的に指定します
+  outputFileTracingRoot: resolve(__dirname),
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias ?? {}),
